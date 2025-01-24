277d43ba5e6e3f4560a5320b4b342cea2c0da6d4
Takuya Shintani <p2180264@gmail.com>
2025-01-24 11:45:05 +0900

    取得方法をスクレイピングを行わない形に変更

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import time
 import webbrowser
 
 import PySimpleGUI as sg
-from bs4 import BeautifulSoup
 from pip._vendor import requests
 
 # ありがとう
@@ -47,71 +46,101 @@ while True:
         rtm = values["RTM"]
         if not rtm.isdecimal():
             rtm = 0
+        else:
+            rtm = int(rtm)
+
         with open('settings', mode='w') as f:
             f.write(apikey + "\n")
             f.write(values["URL"] + "\n")
-            f.write(values["RTM"])
-            if not values["URL"].isdecimal():
-                idurl = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=" + apikey + "&vanityurl=" + \
-                        values["URL"]
-                response = requests.get(idurl)
-                jsonData = response.json()
-                steamID = jsonData["response"]["steamid"]
-            else:
-                steamID = values["URL"]
-            ownedurl = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + apikey + "&steamid=" + steamID + "&format=json"
-            response = requests.get(ownedurl)
-            jsonData = response.json()
-            appList = list(range(0))
-            for jsonObj in jsonData["response"]["games"]:
-                if int(jsonObj["playtime_forever"]) >= int(rtm):
-                    appList.append(jsonObj["appid"])
-            today = datetime.datetime.today()
-            appLength = len(appList)
-            itemCount = 0
-            filename = "SteamSaleInTheLibrary-" + str(today.year) + str(today.month) + str(today.day) + str(
-                today.hour) + str(today.minute) + str(today.second)
-            with open(filename, mode='x', encoding='UTF-8') as f:
-                for appid in appList:
-                    try:
-                        itemCount += 1
-                        print("in process... " + str(itemCount) + " / " + str(appLength))
-                        window["output"].update("in process... " + str(itemCount) + " / " + str(appLength))
-                        appurl = "https://store.steampowered.com/app/" + str(appid) + "?l=japanese"
-                        html = requests.get(appurl)
-                        soup = BeautifulSoup(html.content, "html.parser")
-                        elems = soup.find_all("div")
-                        appName = discPct = origPri = finalPri = ""
-                        for elem in elems:
-                            try:
-                                string = elem.get("class").pop(0)
-                                if string in "bundle_base_discount":
-                                    break
-                                if string in "apphub_AppName":
-                                    if appName == "":
-                                        appName = elem.string
-                                if string in "discount_pct":
-                                    if discPct == "":
-                                        discPct = elem.string
-                                if string in "discount_original_price":
-                                    if origPri == "":
-                                        origPri = elem.string
-                                if string in "discount_final_price":
-                                    if finalPri == "":
-                                        finalPri = elem.string
-                            except:
-                                pass
-                        if appName == "":
-                            print("InAccessible Game ID:" + str(appid))
-                        else:
-                            if discPct != "":
-                                f.write(appName + " : [" + discPct + "] " + origPri + " -> " + finalPri + "\n")
-                                print(appName + " : [" + discPct + "] " + origPri + " -> " + finalPri)
-                        time.sleep(1)
-                    except Exception as e:
-                        print("ERROR - " + appName + " (" + str(appid) + ") ex:" + str(e))
-                        pass
-                subprocess.Popen([r'notepad.exe', filename])
+            f.write(str(rtm))
+
+        # SteamID の取得（Vanity URL か数字の ID か判定）
+        if not values["URL"].isdecimal():
+            idurl = (
+                "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/"
+                f"?key={apikey}&vanityurl={values['URL']}"
+            )
+            response = requests.get(idurl)
+            json_data = response.json()
+            steamID = json_data["response"]["steamid"]
+        else:
+            steamID = values["URL"]
+
+        # ユーザの所有ゲーム一覧を取得
+        ownedurl = (
+            "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/"
+            f"?key={apikey}&steamid={steamID}&format=json"
+        )
+        response = requests.get(ownedurl)
+        json_data = response.json()
+
+        # 指定プレイ時間以上のゲームの appid を抽出
+        appList = []
+        for g in json_data["response"]["games"]:
+            if g["playtime_forever"] >= rtm:
+                appList.append(g["appid"])
+
+        today = datetime.datetime.today()
+        filename = (
+            "SteamSaleInTheLibrary-"
+            + today.strftime("%Y%m%d%H%M%S")
+        )
+
+        appLength = len(appList)
+        itemCount = 0
+
+        with open(filename, mode='x', encoding='UTF-8') as f:
+            for appid in appList:
+                itemCount += 1
+                print(f"in process... {itemCount} / {appLength}")
+                window["output"].update(f"in process... {itemCount} / {appLength}")
+
+                try:
+                    # store API による価格・割引情報の取得
+                    # 例: https://store.steampowered.com/api/appdetails?appids=1091500&cc=jp&l=japanese
+                    store_url = (
+                        "https://store.steampowered.com/api/appdetails"
+                        f"?appids={appid}&cc=jp&l=japanese"
+                    )
+                    store_res = requests.get(store_url)
+                    store_json = store_res.json()
+
+                    if str(appid) not in store_json:
+                        print(f"Invalid response for appid={appid}")
+                        continue
+
+                    app_info = store_json[str(appid)]
+                    if not app_info.get('success'):
+                        print(f"Failed to get data for appid={appid}")
+                        continue
+
+                    data = app_info.get('data', {})
+                    appName = data.get('name', '')
+                    
+                    # 価格情報
+                    price_info = data.get('price_overview')
+                    if not price_info:
+                        continue
+
+                    discount = price_info.get('discount_percent', 0)
+                    initial_price = price_info.get('initial_formatted', '')
+                    final_price = price_info.get('final_formatted', '')
+
+                    if discount > 0:
+                        f.write(
+                            f"{appName} : [-{discount}%] "
+                            f"{initial_price} -> {final_price}\n"
+                        )
+                        print(f"{appName} : [-{discount}%] {initial_price} -> {final_price}")
+
+                except Exception as e:
+                    print(f"ERROR - appid={appid} ex:{e}")
+                    continue
+
+        # 処理終了後にファイルをメモ帳で開く
+        subprocess.Popen(['notepad.exe', filename])
+        print(f"Done! {filename}")
+        window["output"].update(f"Done! {filename}")
 
     if event == 'Get API Key':
         webbrowser.open("https://steamcommunity.com/dev/apikey")
